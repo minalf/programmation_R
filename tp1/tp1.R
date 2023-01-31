4c64b3d285659b6b90c5b8c954a0f22a479f3778
minalf <123751182+minalf@users.noreply.github.com>
2023-01-31 14:55:48 +0100

    Create tp1.R

---
+++ tp1/tp1.R
@@ -0,0 +1,8 @@
+brutToNET <- function(brut) {
+  if (!is.numeric(brut)) {
+    return("ERROR : type not expected")
+  }
+  cotisations <- brut * 0.22
+  net <- brut - cotisations
+  return(net)
+}      
