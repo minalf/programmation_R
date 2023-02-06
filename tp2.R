3c97a636b9fbac6cc9f52e90f869593d13e61d6a
minalf <123751182+minalf@users.noreply.github.com>
2023-02-06 17:00:17 +0100

    Create tp2.R

---
+++ tp2.R
@@ -0,0 +1,21 @@
+readxl(pokemon.xlsx)
+
+dim(x = pokemon)
+
+colnames(x = pokemon)
+
+str(pokemon)
+
+pokemon$generation <- as.factor(x = pokemon$generation)
+class(x = pokemon$generation )
+
+pokemon$is_legendary <- as.factor(x = pokemon$is_legendary)
+class(x = pokemon$is_legendary )
+
+pokemon$type <- as.factor(x = pokemon$type)
+class(x = pokemon$type )
+
+length(levels(pokemon$generation))
+length(levels(pokemon$is_legendary))
+length(levels(pokemon$type))
+
